47c53163991b66d71c528a50d9cf8877ced28570
allitov <litovkinalexan@gmail.com>
2023-11-21 14:40:00 +0700

    Add database

---
+++ docker/init.sql
@@ -0,0 +1,35 @@
+CREATE SCHEMA IF NOT EXISTS news_schema;
+
+CREATE TABLE IF NOT EXISTS news_schema.users (
+    id BIGSERIAL PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    registration_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS news_schema.news_categories (
+    id BIGSERIAL PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS news_schema.news (
+    id BIGSERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    last_update TIMESTAMP NOT NULL,
+    author_id BIGINT NOT NULL,
+    category_id BIGINT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES news_schema.users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES news_schema.news_categories(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS news_schema.comments (
+    id BIGSERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    last_update TIMESTAMP NOT NULL,
+    news_id BIGINT NOT NULL,
+    author_id BIGINT NOT NULL,
+    FOREIGN KEY (news_id) REFERENCES news_schema.news(id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES news_schema.users(id) ON DELETE CASCADE
+);
