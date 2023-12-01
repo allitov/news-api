5e1cb399668995168e955405153cb78a068e6713
allitov <litovkinalexan@gmail.com>
2023-12-01 14:30:15 +0700

    Add news request validation tests

--- src/test/java/com/allitov/newsapi/web/controller/NewsControllerTest.java
+++ src/test/java/com/allitov/newsapi/web/controller/NewsControllerTest.java
@@ -13,6 +13,9 @@ import com.allitov.newsapi.web.mapper.NewsMapper;
 import jakarta.persistence.EntityNotFoundException;
 import net.javacrumbs.jsonunit.JsonAssert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -21,6 +24,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class NewsControllerTest extends AbstractControllerTest {
 
@@ -175,5 +179,135 @@ public class NewsControllerTest extends AbstractControllerTest {
         String expectedResponse = TestUtils.readStringFromResource("response/news/method/news_by_id_not_found_response.json");
 
         JsonAssert.assertJsonEquals(expectedResponse, actualResponse);
+    }
+
+    @ParameterizedTest
+    @MethodSource("blankContent")
+    public void whenCreateNewsWithBlankContent_thenReturnError(String content) throws Exception {
+        NewsRequest request = new NewsRequest();
+        request.setContent(content);
+        request.setAuthorId(1L);
+        request.setCategoryId(1L);
+
+        String actualResponse = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/news")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String expectedResponse = TestUtils.readStringFromResource("response/news/request/blank_news_content_response.json");
+
+        JsonAssert.assertJsonEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void whenCreateNewsWithNullAuthorId_thenReturnError() throws Exception {
+        NewsRequest request = new NewsRequest();
+        request.setContent("content");
+        request.setAuthorId(null);
+        request.setCategoryId(1L);
+
+        String actualResponse = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/news")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String expectedResponse = TestUtils.readStringFromResource("response/news/request/null_news_author_id_response.json");
+
+        JsonAssert.assertJsonEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void whenCreateNewsWithNullCategoryId_thenReturnError() throws Exception {
+        NewsRequest request = new NewsRequest();
+        request.setContent("content");
+        request.setAuthorId(1L);
+        request.setCategoryId(null);
+
+        String actualResponse = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/news")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String expectedResponse = TestUtils.readStringFromResource("response/news/request/null_news_category_id_response.json");
+
+        JsonAssert.assertJsonEquals(expectedResponse, actualResponse);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidAuthorId")
+    public void whenCreateNewsWithInvalidAuthorId_thenReturnError(Long authorId) throws Exception {
+        NewsRequest request = new NewsRequest();
+        request.setContent("content");
+        request.setAuthorId(authorId);
+        request.setCategoryId(1L);
+
+        String actualResponse = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/news")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String expectedResponse = TestUtils.readStringFromResource("response/news/request/invalid_news_author_id_response.json");
+
+        JsonAssert.assertJsonEquals(expectedResponse, actualResponse);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidCategoryId")
+    public void whenCreateNewsWithInvalidCategoryId_thenReturnError(Long categoryId) throws Exception {
+        NewsRequest request = new NewsRequest();
+        request.setContent("content");
+        request.setAuthorId(1L);
+        request.setCategoryId(categoryId);
+
+        String actualResponse = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/news")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        String expectedResponse = TestUtils.readStringFromResource("response/news/request/invalid_news_category_id_response.json");
+
+        JsonAssert.assertJsonEquals(expectedResponse, actualResponse);
+    }
+
+    private static Stream<Arguments> blankContent() {
+        return Stream.of(
+                null,
+                Arguments.of(""),
+                Arguments.of(" ")
+        );
+    }
+
+    private static Stream<Arguments> invalidAuthorId() {
+        return Stream.of(
+                Arguments.of(0L),
+                Arguments.of(-10L)
+        );
+    }
+
+    private static Stream<Arguments> invalidCategoryId() {
+        return Stream.of(
+                Arguments.of(0L),
+                Arguments.of(-40L)
+        );
     }
 }
