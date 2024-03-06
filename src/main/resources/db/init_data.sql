c299cd4bf2faf3722398791035676f11a0165b46
allitov <litovkinalexan@gmail.com>
2024-03-06 17:52:38 +0700

    Change init_data.sql

--- src/main/resources/db/init_data.sql
+++ src/main/resources/db/init_data.sql
@@ -1,12 +1,14 @@
 DROP TABLE IF EXISTS news_schema.comments;
 DROP TABLE IF EXISTS news_schema.news;
 DROP TABLE IF EXISTS news_schema.news_categories;
+DROP TABLE IF EXISTS news_schema.user_roles;
 DROP TABLE IF EXISTS news_schema.users;
 
 CREATE TABLE IF NOT EXISTS news_schema.users (
     id BIGSERIAL PRIMARY KEY,
-    user_name VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL,
     registration_date TIMESTAMP NOT NULL
 );
 
@@ -37,11 +39,19 @@ CREATE TABLE IF NOT EXISTS news_schema.comments (
     FOREIGN KEY (author_id) REFERENCES news_schema.users(id) ON DELETE CASCADE
 );
 
-insert into news_schema.users (user_name, email, registration_date) values ('Garek Simper', 'gsimper0@dropbox.com', '2023-06-30 15:03:20');
-insert into news_schema.users (user_name, email, registration_date) values ('Nial Lodemann', 'nlodemann1@state.gov', '2023-01-28 03:00:27');
-insert into news_schema.users (user_name, email, registration_date) values ('Emmey Crossland', 'ecrossland2@canalblog.com', '2023-06-08 19:53:03');
-insert into news_schema.users (user_name, email, registration_date) values ('Chas Sapshed', 'csapshed3@noaa.gov', '2022-12-13 01:10:09');
-insert into news_schema.users (user_name, email, registration_date) values ('Fina Sugden', 'fsugden4@nymag.com', '2023-04-14 06:02:56');
+CREATE TABLE IF NOT EXISTS news_schema.user_roles (
+    user_id BIGINT NOT NULL,
+    roles VARCHAR(20) NOT NULL,
+    CHECK (((roles)::text = ANY ((ARRAY['USER'::character varying, 'ADMIN'::character varying, 'MODERATOR'::character varying])::text[]))),
+    PRIMARY KEY (user_id, roles),
+    FOREIGN KEY (user_id) REFERENCES news_schema.users(id) ON DELETE CASCADE
+);
+
+insert into news_schema.users (username, email, password, registration_date) values ('Garek Simper', 'gsimper0@dropbox.com', '$2a$04$5ZDXOb3IHiy5w4laMT7vh.umbLCzxNkT3xJMAm5zo0oi0idceAtca', '2023-06-30 15:03:20');
+insert into news_schema.users (username, email, password, registration_date) values ('Nial Lodemann', 'nlodemann1@state.gov', '$2a$04$AzvbtdT2NT0ZrdlDakJgduIstaoKmgClLWWAplob4PHHzy9RLDRUi', '2023-01-28 03:00:27');
+insert into news_schema.users (username, email, password, registration_date) values ('Emmey Crossland', 'ecrossland2@canalblog.com', '$2a$04$rqJC3hO/U8pubX4s9KypqejEbhMSlnxp16jtJsVr1soQT8hrSC5dK', '2023-06-08 19:53:03');
+insert into news_schema.users (username, email, password, registration_date) values ('Chas Sapshed', 'csapshed3@noaa.gov', '$2a$04$EylmMDBQWbcA1Vi0q8msQOog7ROi70WiKDhno.jsHvvErI.0vZU.O', '2022-12-13 01:10:09');
+insert into news_schema.users (username, email, password, registration_date) values ('Fina Sugden', 'fsugden4@nymag.com', '$2a$04$f18HajB3iSvy0dXqS403UuULcxm624fmwMzMSQ8qaJl3xRRcUw4Yu', '2023-04-14 06:02:56');
 
 insert into news_schema.news_categories (category_name) values ('CDL');
 insert into news_schema.news_categories (category_name) values ('PK/PD');
@@ -60,3 +70,12 @@ insert into news_schema.comments (content, creation_date, last_update, news_id, 
 insert into news_schema.comments (content, creation_date, last_update, news_id, author_id) values ('Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', '2023-01-16 18:16:05', '2023-10-02 17:34:20', 1, 3);
 insert into news_schema.comments (content, creation_date, last_update, news_id, author_id) values ('In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', '2023-10-15 11:21:35', '2023-02-09 19:41:29', 5, 4);
 insert into news_schema.comments (content, creation_date, last_update, news_id, author_id) values ('Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', '2023-08-12 07:31:52', '2023-06-03 21:50:53', 1, 1);
+
+insert into news_schema.user_roles (user_id, roles) values (1, 'ADMIN');
+insert into news_schema.user_roles (user_id, roles) values (1, 'MODERATOR');
+insert into news_schema.user_roles (user_id, roles) values (1, 'USER');
+insert into news_schema.user_roles (user_id, roles) values (2, 'MODERATOR');
+insert into news_schema.user_roles (user_id, roles) values (2, 'USER');
+insert into news_schema.user_roles (user_id, roles) values (3, 'USER');
+insert into news_schema.user_roles (user_id, roles) values (4, 'USER');
+insert into news_schema.user_roles (user_id, roles) values (5, 'USER');
