f515a4ab1e057330e5cc2c4e82b442f2dbae7056
allitov <litovkinalexan@gmail.com>
2024-03-13 16:23:35 +0700

    Add documentation for news controller

--- src/main/java/com/allitov/newsapi/web/controller/v2/NewsController.java
+++ src/main/java/com/allitov/newsapi/web/controller/v2/NewsController.java
@@ -4,13 +4,23 @@ import com.allitov.newsapi.model.data.News;
 import com.allitov.newsapi.model.service.NewsService;
 import com.allitov.newsapi.security.UserDetailsImpl;
 import com.allitov.newsapi.web.dto.request.news.NewsRequest;
+import com.allitov.newsapi.web.dto.response.error.ErrorResponse;
 import com.allitov.newsapi.web.dto.response.news.NewsListResponse;
 import com.allitov.newsapi.web.dto.response.news.NewsResponse;
 import com.allitov.newsapi.web.filter.NewsFilter;
 import com.allitov.newsapi.web.mapper.NewsMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -27,16 +37,162 @@ public class NewsController {
 
     private final NewsMapper newsMapper;
 
+    @Operation(
+            summary = "Get news by filter",
+            description = "Get news by filter. " +
+                    "Returns a list of news matching the filter parameters. " +
+                    "Requires any of the authorities: ['ADMIN', 'MODERATOR', 'USER']",
+            security = @SecurityRequirement(name = "Basic authorisation")
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 200 and news list if everything completed successfully",
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = NewsListResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 400 and error message if filter has invalid values",
+                    responseCode = "400",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 401 and error message if user is not authorized",
+                    responseCode = "401",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 403 and error message if user has no required authorities",
+                    responseCode = "403",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @GetMapping("/filter")
-    public ResponseEntity<NewsListResponse> filterBy(@Valid NewsFilter filter) {
+    public ResponseEntity<NewsListResponse> filterBy(@ParameterObject @Valid NewsFilter filter) {
         return ResponseEntity.ok(newsMapper.newsListToNewsListResponse(newsService.filterBy(filter)));
     }
 
+    @Operation(
+            summary = "Get news by id",
+            description = "Get news by id. Returns news with requested id. " +
+                    "Requires any of the authorities: ['ADMIN', 'MODERATOR', 'USER']",
+            parameters = {
+                    @Parameter(name = "id", example = "1")
+            },
+            security = @SecurityRequirement(name = "Basic authorisation")
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 200 and news if everything completed successfully",
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = NewsResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 401 and error message if user is not authorized",
+                    responseCode = "401",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 403 and error message if user has no required authorities",
+                    responseCode = "403",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 404 and error message if news with requested id was not found",
+                    responseCode = "404",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @GetMapping("/{id}")
     public ResponseEntity<NewsResponse> findById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(newsMapper.newsToResponse(newsService.findById(id)));
     }
 
+    @Operation(
+            summary = "Create news",
+            description = "Create news. Returns status 201 and created news location. " +
+                    "Requires any of the authorities: ['ADMIN', 'MODERATOR', 'USER']",
+            security = @SecurityRequirement(name = "Basic authorisation")
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 201 and created news location " +
+                            "if everything completed successfully",
+                    responseCode = "201",
+                    headers = {
+                            @Header(name = "Location", description = "Created news location")
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 400 and error message if request has invalid values",
+                    responseCode = "400",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 401 and error message if user is not authorized",
+                    responseCode = "401",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 403 and error message if user has no required authorities",
+                    responseCode = "403",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @PostMapping
     public ResponseEntity<Void> create(@Valid @RequestBody NewsRequest request,
                                        @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -47,6 +203,61 @@ public class NewsController {
         return ResponseEntity.created(URI.create("/api/v2/news/" + savedNews.getId())).build();
     }
 
+    @Operation(
+            summary = "Update news by id",
+            description = "Update news by id. Returns status 204. " +
+                    "Requires any of the authorities: ['ADMIN', 'MODERATOR', 'USER']",
+            parameters = {
+                    @Parameter(name = "id", example = "1")
+            },
+            security = @SecurityRequirement(name = "Basic authorisation")
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 204 if everything completed successfully",
+                    responseCode = "204"
+            ),
+            @ApiResponse(
+                    description = "Returns status 400 and error message if request has invalid values",
+                    responseCode = "400",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 401 and error message if user is not authorized",
+                    responseCode = "401",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 403 and error message if user has no required authorities",
+                    responseCode = "403",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 404 and error message if news with requested id was not found",
+                    responseCode = "404",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateById(@PathVariable("id") Long id,
                                            @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -56,6 +267,51 @@ public class NewsController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Delete news by id",
+            description = "Delete news by id. Returns status 204. " +
+                    "Requires any of the authorities: ['ADMIN', 'MODERATOR', 'USER']",
+            parameters = {
+                    @Parameter(name = "id", example = "1")
+            },
+            security = @SecurityRequirement(name = "Basic authorisation")
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "Returns status 204 if everything completed successfully",
+                    responseCode = "204"
+            ),
+            @ApiResponse(
+                    description = "Returns status 401 and error message if user is not authorized",
+                    responseCode = "401",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 403 and error message if user has no required authorities",
+                    responseCode = "403",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    description = "Returns status 404 and error message if news with requested id was not found",
+                    responseCode = "404",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ErrorResponse.class),
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable("id") Long id,
                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
