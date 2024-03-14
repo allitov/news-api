62077afe5ab0a028e9712f8da1baa458f7906d61
allitov <litovkinalexan@gmail.com>
2024-03-14 21:12:04 +0700

    Add testcontainers

--- src/test/java/com/allitov/newsapi/NewsApiApplicationTests.java
+++ src/test/java/com/allitov/newsapi/NewsApiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.allitov.newsapi;
 
+import com.allitov.testutils.EnableTestcontainers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableTestcontainers
 class NewsApiApplicationTests {
 
     @Test
